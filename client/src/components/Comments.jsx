993f5fd090f0b0b0b5dc37868a169c1beb99b464
Christopher <christopherperez@Christophers-Laptop.local>
2023-08-08 15:07:25 -0500

    updated comments.jsx

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,11 +1,10 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles.js";
 import { SectionWrapper } from "../hoc/index.js";
 import { slideIn } from "../utils/motion.js";
-
+// import { comments } from "/"
 
 const Comments = () => {
     const formRef = useRef();
@@ -31,35 +30,26 @@ const Comments = () => {
         e.preventDefault();
         setLoading(true);
 
-        emailjs
-            .send(
-                import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-                import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-                {
-                    from_name: form.name,
-                    to_name: "JavaScript Mastery",
-                    from_email: form.email,
-                    to_email: "sujata@jsmastery.pro",
-                    message: form.message,
-                },
-                import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-            )
-            .then(
-                () => {
-                    setLoading(false);
-                    alert("Thank you. I will get back to you as soon as possible.");
+        axios.post('/api/comments', form, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                setLoading(false);
+                alert('Thank you. Your comment has been submitted.');
 
-                    setForm({
-                        name: "",
-                        email: "",
-                        message: "",
-                    });
-                },
+                setForm({
+                    name: '',
+                    email: '',
+                    message: '',
+                });
+            },
                 (error) => {
                     setLoading(false);
                     console.error(error);
 
-                    alert("Ahh, something went wrong. Please try again.");
+                    alert('Ahh, something went wrong. Please try again.');
                 }
             );
     };
